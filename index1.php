2778827be5bc3ae982385552bb9d209673a380a8
yeg <yegking@163.com>
2015-11-25 22:00:37 +0800

    第一次改版
    第一次该头和尾

---
+++ index1.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Insert title here</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/common1.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<?php
+require_once "common1.html";
+echo "common1.html";
+?>
+
+</body>
+</html>
